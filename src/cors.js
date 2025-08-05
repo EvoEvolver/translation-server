83a9a45553f236005374afd5f8eda66423bf148b
Zijian Zhang <42955607+doomspec@users.noreply.github.com>
2025-08-05 18:08:59 -0400

    Update cors.js

--- src/cors.js
+++ src/cors.js
@@ -1,28 +1,30 @@
 const config = require('config');
 
 module.exports = async (ctx, next) => {
-	headersSet = {};
-	function set(key, value) {
-		ctx.set(key, value);
-		headersSet[key] = value;
-	}
-	if (ctx.headers.origin) {
-		let allowedOrigins = config.get('allowedOrigins').filter(x => x);
-		let allAllowed = allowedOrigins.includes('*');
-		if (allAllowed || allowedOrigins.includes(ctx.headers.origin)) {
-			set("Access-Control-Allow-Origin", allAllowed ? '*' : ctx.headers.origin);
-			set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-			set("Access-Control-Allow-Headers", "Content-Type");
-			set("Access-Control-Expose-Headers", "Link");
-		}
-		// Force a 200 on API Gateway
-		ctx.body = '';
-	}
-	try {
-		await next();
-	}
-	catch (e) {
-        e.headers = Object.assign({}, e.headers, headersSet);
-        throw e;
-    }
+  const headersSet = {};
+
+  function set(key, value) {
+    ctx.set(key, value);
+    headersSet[key] = value;
+  }
+
+  // Always allow any origin
+  set("Access-Control-Allow-Origin", "*");
+  set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  set("Access-Control-Allow-Headers", "Content-Type");
+  set("Access-Control-Expose-Headers", "Link");
+
+  // Optionally respond to OPTIONS preflight
+  if (ctx.method === "OPTIONS") {
+    ctx.status = 200;
+    ctx.body = '';
+    return;
+  }
+
+  try {
+    await next();
+  } catch (e) {
+    e.headers = Object.assign({}, e.headers, headersSet);
+    throw e;
+  }
 };
